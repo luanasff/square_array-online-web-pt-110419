02f3c7742dd2560824f4048c492dd94eed536d2a
Luana Faria Lane <luanasfff@gmail.com>
2019-11-11 01:16:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
 
-square_array = array.each{x**}
- new_array = Array.new
-  array.each { |value| new_array << value ** 2 }
-  new_array
+
+square_array= array.each do { |value| new_array << value ** 2 }
+  
 
 end
